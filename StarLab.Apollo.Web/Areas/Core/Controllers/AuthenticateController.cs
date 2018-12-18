6e00940ad7c7a180dbc33fb0d3ecde57cc7a4259
Jonas <xqyi@hotmail.com>
2018-12-18 13:11:22 +0800

    test

---
+++ StarLab.Apollo.Web/Areas/Core/Controllers/AuthenticateController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StarLab.Apollo.Web.Areas.Core.Controllers
+{
+    public class AuthenticateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
